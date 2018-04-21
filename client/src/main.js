16ebb1f173ce90936bdf3bf4d579e995f24074c8
Flaise <x.Flaise@gmail.com>
2018-04-21 10:47:45 -0500

    Client opens websocket connection to server.

--- client/src/main.js
+++ client/src/main.js
@@ -5,5 +5,9 @@ const {start} = require('skid/lib/load');
 
 start(true);
 
+const {addHandler} = require('skid/lib/event');
+const io = require('socket.io-client');
 
-require('socket.io-client');
+addHandler('load_done', (state) => {
+    const socket = io('http://localhost:3000', {transports: ['websocket']});
+});
--- server/server.js
+++ server/server.js
@@ -1,15 +1,16 @@
-// var app = require('express')();
-var http = require('http').Server();
-var io = require('socket.io')(http);
+const http = require('http');
+const io = require('socket.io');
 
-// app.get('/', function(req, res){
-//   res.sendFile(__dirname + '/index.html');
-// });
+const server = http.Server();
+const socket = io(server);
 
-io.on('connection', function(socket) {
+socket.on('connection', function(socket){
     console.log('a user connected');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
 });
 
-http.listen(3000, function() {
+server.listen(3000, function() {
     console.log('listening on *:3000');
 });
